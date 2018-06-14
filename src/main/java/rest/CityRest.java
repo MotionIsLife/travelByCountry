be7f11d938f925ac9a1b4cc7618e5abc080f56c7
kprikhodko <1_v_pole@inbox.ru>
2018-06-14 22:35:03 +0300

    Добавлен пример PostMapping

--- src/main/java/rest/CityRest.java
+++ src/main/java/rest/CityRest.java
@@ -1,43 +1,24 @@
 package rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import vo.City;
 
-import static org.springframework.web.bind.annotation.RequestMethod.GET;
-import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
 @RestController
-@RequestMapping(value = "/city"/*, consumes = MediaType.APPLICATION_JSON_UTF8_VALUE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE*/)
+@RequestMapping(value = "/city")
 public class CityRest {
 
     @Autowired
     private City city;
 
-    @RequestMapping(value = "/getById", method = RequestMethod.POST)
-    public @ResponseBody City getCityById(@RequestBody Long id){
-        return new City(id);
-    }
-
-    @RequestMapping(value = "/hello2", method = RequestMethod.POST)
-    public @ResponseBody City getHello2(@PathVariable Long id){
-        return new City(id);
-    }
-
-    @RequestMapping(value = "/hello", method = GET)
-    public String getHello() {
-        return "hello";
-    }
-
-    @PostMapping(value = "/helloPost", consumes = MediaType.APPLICATION_JSON_UTF8_VALUE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    @ResponseBody
-    public City getHelloPost(@RequestBody City city) {
+    @RequestMapping(value = "/addCity", method = RequestMethod.POST)
+    public @ResponseBody City addAlbum(@RequestBody City city){
         return city;
     }
 
-    @RequestMapping(value = "/addCity", method = RequestMethod.POST)
-    public @ResponseBody City addAlbum(@RequestBody City city){
+    @PostMapping(value = "/addCityPost")
+    public @ResponseBody City addAlbumPost(@RequestBody City city){
         return city;
     }
 }
